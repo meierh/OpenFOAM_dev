795e057f83cb9adbb211a91075ba3725f306db50
Helge_Meier <hmelbtal@t-online.de>
2022-10-31 23:43:54 +0100

    Cont NurbsStructure

---
+++ src/nurbsStructures/NurbsStructure.C
@@ -0,0 +1,49 @@
+#include "NurbsStructure.H"
+
+Foam::NurbsStructure::NurbsStructure
+(
+    Time& runTime
+):
+runTime(runTime)
+{
+    runDirectory = runTime.rootPath();
+    caseName = runTime.caseName();
+    xmlPath = getXMLPath();
+}
+
+Foam::word Foam::NurbsStructure::getXMLPath()
+{
+    fileName caseDirectory = runDirectory+"/"+caseName;
+    fileName constantDirectory = caseDirectory;
+
+    DIR  *dir = NULL;
+    const char *pathConstantDirectory = constantDirectory.c_str();
+    dir = opendir(pathConstantDirectory);
+    if(dir==NULL)
+        FatalIOError<<"Error reading Nurbs file. Could not open the directory!"<<exit(FatalIOError);
+    DynamicList<word> directoryFiles;
+    struct dirent *dp;
+    while((dp=readdir(dir))!=NULL)
+    {
+        if(dp->d_name==NULL)
+            FatalIOError<<"Reading existing files name as null pointer!"<<exit(FatalIOError);
+        directoryFiles.append(word(dp->d_name));
+    }
+    DynamicList<word> xmlFiles;
+    for(int i=0;i<directoryFiles.size();i++)
+    {
+        std::size_t fileEndingStart = directoryFiles[i].rfind(".");
+        if(fileEndingStart==std::string::npos)
+            continue;
+        word fileEnding = directoryFiles[i].substr(fileEndingStart,directoryFiles[i].size()-fileEndingStart);
+        if(fileEnding.compare(".xml")==0)
+            xmlFiles.append(directoryFiles[i]);
+    }
+    if(xmlFiles.size()==0)
+        FatalIOError<<"No Nurbs file found!"<<exit(FatalIOError);
+    if(xmlFiles.size()>1)
+        Info<<"Multiple Nurbs files found. First one will be used!"<<endl;
+    word fullPath = constantDirectory+"/"+xmlFiles[0];
+    
+    return fullPath;
+}
