e7b8f34d210a427c22c43cf094da94e19fffe5e0
Helge_Meier <hmelbtal@t-online.de>
2022-11-01 22:03:45 +0100

    Cont NurbsStructure

--- src/nurbsStructures/NurbsStructure.C
+++ src/nurbsStructures/NurbsStructure.C
@@ -9,6 +9,13 @@ runTime(runTime)
     runDirectory = runTime.rootPath();
     caseName = runTime.caseName();
     xmlPath = getXMLPath();
+    
+    cntOpt.ptsType = 2;
+    cntOpt.ptsN = 2;
+    cntOpt.csFac = 2.0;
+    cntOpt.preg = 0.02 * geoR0; 
+    cntOpt.kc = 0.2 * geoE0*geoR0*geoR0; 
+    cntOpt.initOut = 0;
 }
 
 Foam::word Foam::NurbsStructure::getXMLPath()
@@ -43,7 +50,235 @@ Foam::word Foam::NurbsStructure::getXMLPath()
         FatalIOError<<"No Nurbs file found!"<<exit(FatalIOError);
     if(xmlFiles.size()>1)
         Info<<"Multiple Nurbs files found. First one will be used!"<<endl;
-    word fullPath = constantDirectory+"/"+xmlFiles[0];
-    
+    word fullPath = constantDirectory+"/"+xmlFiles[0];    
+    name = xmlFiles[0].erase(xmlFiles[0].length()-4,std::string::npos);
     return fullPath;
+}
+
+void Foam::NurbsStructure::createNurbsStructure()
+{
+// **** Loading curves from file **** //
+	std::string name = "BCC_3x3x3_L20";
+    std::string rodsXMLFilePath = xmlPath;
+    std::vector<gsNurbs<double>> rodsList;
+    ActiveRodMesh::import_xmlCrv(rodsList, rodsXMLFilePath, 3, 1, 0);
+    const int	nR = rodsList.size();
+    std::string folder = runDirectory+"/"+caseName;
+    
+	if (_mkdir(("..\\output\\" + name).c_str()) != 0 && errno != EEXIST) throw("Problem making folder!");
+	if (_mkdir(folder.c_str()) != 0) throw("Problem making folder!");
+// **** End **** //
+    
+// **** Create initial geometry **** //
+    double	x0 = 1e6, x1 = -1e6, y0 = 1e6, y1 = -1e6, z0 = 1e6, z1 = -1e6;
+    for(int i=0; i<nR; i++)
+    {
+        for (int j=0; j<rodsList[i].coefs().rows(); j++)
+        {
+            for (int k=0; k<3; k++)
+            {
+                rodsList[i].coefs()(j, k) = 1e-6 * round(1e6 * rodsList[i].coefs()(j, k));
+            }
+        }
+        x0 = std::fmin(x0, rodsList[i].coefs().topRows(0).coeff(0, 0));
+        x0 = std::fmin(x0, rodsList[i].coefs().bottomRows(1).coeff(0, 0));
+        x1 = std::fmax(x1, rodsList[i].coefs().topRows(0).coeff(0, 0));
+        x1 = std::fmax(x1, rodsList[i].coefs().bottomRows(1).coeff(0, 0));
+        y0 = std::fmin(y0, rodsList[i].coefs().topRows(0).coeff(0, 1));
+        y0 = std::fmin(y0, rodsList[i].coefs().bottomRows(1).coeff(0, 1));
+        y1 = std::fmax(y1, rodsList[i].coefs().topRows(0).coeff(0, 1));
+        y1 = std::fmax(y1, rodsList[i].coefs().bottomRows(1).coeff(0, 1));
+        z0 = std::fmin(z0, rodsList[i].coefs().topRows(0).coeff(0, 2));
+        z0 = std::fmin(z0, rodsList[i].coefs().bottomRows(1).coeff(0, 2));
+        z1 = std::fmax(z1, rodsList[i].coefs().topRows(0).coeff(0, 2));
+        z1 = std::fmax(z1, rodsList[i].coefs().bottomRows(1).coeff(0, 2));
+    }
+    gsVector<double,3> latSize;
+    latSize << x1 - x0, y1 - y0, z1 - z0;
+    
+    gsVector<double,3> dX;
+    dX << -x0, -y0, -z0;
+    for (int i = 0; i < nR; i++)
+    {
+        rodsList[i].knots().transform(0., 1.);	// re-scale knot vector
+        rodsList[i].translate(dX);				// translate to 0
+        rodsList[i].scale(latScale);
+    }
+    latSize *= latScale;
+    printf("Rods:  %i\n", nR);
+    printf("Dimensions: %4.1fx%4.1fx%4.1f mm\n", latSize[0], latSize[1], latSize[2]);
+// **** End **** //
+    
+// **** Create rods and mesh **** //
+    // * Base NURBS curve - straight line as B-Spline
+    const int	el = 1;
+    const int	p = 1;
+    const int	n = p + el;
+    gsKnotVector<double> knots(0, 1, el - 1, p + 1, 1, p);
+    gsMatrix<double> weights(n, 1);
+    weights.setOnes();
+
+    // * CS input vectors
+    gsVector<double> csE(1);
+    csE << geoE0;
+    gsVector<double> csR(1);
+    csR << geoR0;
+
+    // * CS basis for optimization
+    const int ep = 1;
+    const int ee = 1;
+    const int ekn_n = ep + ee;
+    const int ekn_mi = ee - 1;
+    gsKnotVector<double> ekn(0., 1., ekn_mi, ep + 1, 1, ep);
+    gsMatrix<double> ewts(ekn_n, 1);
+    ewts.setOnes();
+
+    // * Basic classes
+    gsVector<double, 3> gVec;
+    gVec.setZero();
+    gVec(latDir) = -9.8;
+    gsConstantFunction<double>* loadG;
+    loadG = new gsConstantFunction<double>(gVec, 1);
+    gsConstantFunction<double>* twist = new gsConstantFunction<double>(0., 1);
+    std::vector< ActiveRodMesh::rodCrossSection* >	 Geo(nR);
+    std::vector< ActiveRodMesh::rodCosserat* >		 Rods(nR);
+    std::vector< gsNurbsBasis<double>* >	 BasisRef(nR);
+    
+    // * Make rods
+    printf("Make rods ... \n");
+    #ifdef _OPENMP
+    omp_set_num_threads(omp_nthreads);
+    #endif	
+
+    #pragma omp parallel for  
+    for (int i = 0; i < nR; i++)
+    {
+        // CS Geometry
+        Geo[i] = new ActiveRodMesh::rodCScircle(csE, csR, geoNu, geoRho, Temp0, 0, plot_c);
+
+        // Basis refine
+        BasisRef[i] = rodsList[i].basis().clone();
+        BasisRef[i]->degreeElevate(p_sim - BasisRef[i]->degree());
+        BasisRef[i]->uniformRefine(el_sim - BasisRef[i]->numElements());
+
+        // Make rod
+        if (use_mixed)
+            Rods[i] = new ActiveRodMesh::rodCosseratMixed(rodsList[i], *BasisRef[i], *Geo[i], twist, 2, 0);
+        else
+            Rods[i] = new ActiveRodMesh::rodCosserat(rodsList[i], *BasisRef[i], *Geo[i], twist, 2, 0);
+
+        // Cross-section basis for optimization
+        Rods[i]->resetEbasis(ekn, ewts);
+
+        // Gravity 
+        if (applyGravity)
+            Rods[i]->set_force_lG(loadG);
+    }
+
+    // * Boundary conditions
+    printf("Boundary conditions ... \n");
+
+    std::vector<bool> uBC0(3, false);
+    std::vector<bool> uBCxz(3, false);
+    std::vector<bool> uBCyz(3, false);
+    std::vector<bool> uBCx(3, true);
+    std::vector<bool> uBCy(3, true);
+    std::vector<bool> uBCz(3, true);
+    std::vector<bool> uBCdir(3, true);
+    uBCxz[1] = true;
+    uBCyz[0] = true;
+    uBCx[0] = false;
+    uBCy[1] = false;
+    uBCz[2] = false;
+    uBCdir[latDir] = false;
+    bool rBC0 = false;
+    const double bceps = 1e-5;
+    const int dir1 = (latDir + 1) % 3;
+    const int dir2 = (latDir + 2) % 3;
+    gsVector<double, 3> uBCxV;
+    uBCxV.setZero();
+
+    std::vector<int>	pp_rid(0);
+    std::vector<bool>	pp_eid(0);
+    std::vector<double>		pp_wts(0);
+
+    if (bcMode == 0)
+    {
+        for (int i = 0; i < nR; i++)
+        {
+            if (Rods[i]->m_eval_Crv.leftCols(1)(latDir, 0) < bceps * latSize(latDir))
+            {
+                Rods[i]->setBCu(0, uBC0); 
+                if (bcRot)
+                    Rods[i]->setBCr(0, false);
+            }
+            if (Rods[i]->m_eval_Crv.rightCols(1)(latDir, 0) < bceps * latSize(latDir))
+            {
+                Rods[i]->setBCu(1, uBC0); 
+                if (bcRot)
+                    Rods[i]->setBCr(1, false);
+            }
+        }
+
+        uBCxV(latDir) = loadDir * latSize(latDir) * loadStrain;
+        for (int i = 0; i < nR; i++)
+        {
+            if (Rods[i]->m_eval_Crv.leftCols(1)(latDir, 0) > (1. - bceps) * latSize(latDir))
+            {
+                Rods[i]->setBCu(0, uBC0, uBCxV); //uBCx
+                if (bcRot)
+                    Rods[i]->setBCr(0, false);
+                pp_rid.push_back(i);
+                pp_eid.push_back(0);
+                pp_wts.push_back(1.);
+            }
+
+            if (Rods[i]->m_eval_Crv.rightCols(1)(latDir, 0) > (1. - bceps) * latSize(latDir))
+            {
+                Rods[i]->setBCu(1, uBC0, uBCxV); //uBCx
+                if (bcRot)
+                    Rods[i]->setBCr(1, false);
+                pp_rid.push_back(i);
+                pp_eid.push_back(1);
+                pp_wts.push_back(1.);
+            }
+        }
+    }
+
+    // * Rod mesh
+    printf("Rod mesh ... \n");
+
+    ActiveRodMesh::RodMeshOptions meshOpt;
+    meshOpt.name = name;
+    ActiveRodMesh::rodMesh myMesh(Rods, meshOpt);
+    myMesh.setTemp(Temp0, Temp0);
+
+    // * Output mesh
+    ParaViewOptions vtkOpt;
+    vtkOpt.name = name;
+    vtkOpt.folder = folder;
+    vtkOpt.nPoints = plot_n;
+    vtkOpt.nodeThicken = 0;
+    vtkOpt.solidGeo = plot_vtk_solid;
+
+    if (plot_vtk_geo)
+    {
+        ParaViewOptions vtkOptGeo = vtkOpt;
+        vtkOptGeo.name = name + "_geo";
+        vtkOptGeo.folder = folder + "\\geo";
+        vtkOptGeo.unDeformed = 1;
+        vtkOptGeo.solidGeo = (plot_vtk > 1);
+        if (_mkdir(vtkOptGeo.folder.c_str()) != 0)
+            std::cout << "Problem making folder!\n";
+        myMesh.writeParaView(vtkOptGeo);
+        //myMesh.writeXml(vtkOptGeo.name, vtkOptGeo.folder);
+    }
+    //std::cout << "Total length: " << myMesh.m_initLength << "\n";
+
+    // * Enable contact
+    if (isContact)
+    {
+        printf("Initialize contact ... \n");
+        myMesh.contact_init(cntOpt);
+    }
 }
