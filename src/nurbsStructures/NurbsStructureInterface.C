c24d36318774443c258b441281765ca18785421f
Helge_Meier <hmelbtal@t-online.de>
2022-11-24 12:29:12 +0100

    Combined in NurbsStructureInterface

--- src/nurbsStructures/NurbsStructureInterface.C
+++ src/nurbsStructures/NurbsStructureInterface.C
@@ -1,14 +1,30 @@
-#include "NurbsStructure.H"
+#include "NurbsStructureInterface.H"
 
-Foam::NurbsStructure::NurbsStructure
+Foam::NurbsStructureInterface::NurbsStructureInterface
 (
-    Time& runTime
+    Time& runTime,
+    const dimensionedScalar& alpha,
+    const volScalarField& T,
+    const volScalarField& p,
+    const volVectorField& U,
+    const cutCellFvMesh& mesh,
+    const dimensionedScalar nu,
+    const word& IBpatchName
 ):
 runTime(runTime),
 runDirectory(runTime.rootPath()),
 caseName(runTime.caseName()),
 xmlPath(getXMLPath()),
-nR(loadRodsFromXML())
+nR(loadRodsFromXML()),
+alpha(alpha),
+T(T),
+p(p),
+U(U),
+mesh(mesh),
+IBpatchName(IBpatchName),
+nu(nu),
+meshPointNurbsReference(mesh.getMeshPointNurbsReference()),
+Curves(mesh.getCurves())
 {    
     cntOpt.ptsType = 2;
     cntOpt.ptsN = 2;
@@ -17,10 +33,106 @@ nR(loadRodsFromXML())
     cntOpt.kc = 0.2 * geoE0*geoR0*geoR0; 
     cntOpt.initOut = 0;
     
-    createNurbsStructure();    
+    createNurbsStructure();
+    
+    auto cutCellBound = this->mesh.Sf().boundaryField();
+    const fvBoundaryMesh& bound = mesh.boundary();
+    IBPatchID = bound.findPatchID(IBpatchName);
 }
 
-Foam::word Foam::NurbsStructure::getXMLPath()
+void Foam::NurbsStructureInterface::assignBoundaryFacesToNurbsCurves()
+{
+    const fvBoundaryMesh& boundary = mesh.boundary();
+    const fvPatch& nurbsBoundary = boundary[IBPatchID];
+    const faceList& faces = mesh.faces();
+    label i=0;
+    label faceInd=nurbsBoundary.start();
+    for(label i=0;i<nurbsBoundary.size();i++,faceInd++)
+    {
+        face thisFace = faces[faceInd];
+        for(label j=0;j<thisFace.size();j++)
+        {
+            boundaryPntToFace.insert(std::pair<label,label>(thisFace[j],faceInd));
+        }
+    }
+    
+    nurbsParameterToPnt = List<std::multimap<scalar,label>>(Curves->size());
+    
+    for(auto iterPnt=boundaryPntToFace.begin(); iterPnt!=boundaryPntToFace.end(); ++iterPnt)
+    {
+        label pntLabel = iterPnt->first;
+        const DynamicList<cutCellFvMesh::nurbsReference>&  refs = meshPointNurbsReference[pntLabel];
+        const cutCellFvMesh::nurbsReference& ref = refs[0];
+        nurbsParameterToPnt[ref.nurbsInd].insert(std::pair<scalar,label>(ref.nurbsPara,pntLabel));
+    }
+    
+    for(int nurbsInd=0; nurbsInd<Curves->size(); nurbsInd++)
+    {
+        std::vector<scalar> nurbsParaDistCurve;
+        for(auto iterPara = nurbsParameterToPnt.begin();
+            iterPara != nurbsParameterToPnt.end();
+            ++iterPara)
+        {
+            scalar nurbsParaOne = iterPara->first;
+            face thisFace = faces[iterPara->second];
+            std::vector<scalar> nurbsParaDistFace;
+            for(int i=0;i<thisFace.size();i++)
+            {
+                label facePnt = thisFace[i];
+                const DynamicList<cutCellFvMesh::nurbsReference>&  refs = meshPointNurbsReference[facePnt];
+                for(cutCellFvMesh::nurbsReference& ref : refs)
+                {
+                    if(ref.nurbsInd == nurbsInd)
+                    {
+                        nurbsParaDistFace.push_back(std::abs(ref.nurbsPara-nurbsParaOne));
+                    }
+                }
+            }
+            const auto [min, max] = std::minmax_element(nurbsParaDistFace.begin(),nurbsParaDistFace.end());
+            if(nurbsParaDistFace.size()!=0)
+            {
+                nurbsParaDistCurve.push_back(max);
+            }            
+        }
+        std::sort(nurbsParaDistCurve.begin(),nurbsParaDistCurve.end());
+        label medInd = nurbsParaDistCurve.size()/2
+        scalar medianNurbsParaDist = nurb
+    }
+}
+
+void Foam::NurbsStructureInterface::computeIBHeatFlux()
+{
+    tmp<GeometricField<double,fvsPatchField,surfaceMesh>> gradT = fvc::snGrad(T,IBpatchName);
+    GeometricField<double,fvsPatchField,surfaceMesh>& gTF = gradT.ref();
+    const GeometricField<double,fvsPatchField,surfaceMesh>::Boundary& gradTBoundary = gTF.boundaryField();
+    const fvsPatchField<double> ibgradT = gradTBoundary[IBPatchID];
+    
+    
+    
+    gismo::gsNurbs<double> heatFlux;
+    
+    
+    
+    Info<<"gradT size:"<<gradTBoundary.size()<<endl; 
+    Info<<"ibgradT size:"<<ibgradT.size()<<endl;
+}
+
+void Foam::NurbsStructureInterface::computeIBForce()
+{
+    tmp<GeometricField<Tensor<double>,fvPatchField,volMesh>> gU = fvc::grad(U);
+    GeometricField<Tensor<double>,fvPatchField,volMesh>& gradU = gU.ref();
+    GeometricField<SymmTensor<double>,fvPatchField,volMesh> totalStress = symm(-p*tensor::one + nu*(gradU + gradU.T()));
+    
+    const vectorField& Sfp = mesh.Sf().boundaryField()[IBPatchID];
+    const scalarField& magSfp = mesh.magSf().boundaryField()[IBPatchID];
+    const symmTensorField& totalStressIB = totalStress.boundaryField()[IBPatchID];
+    
+    Field<Vector<double>> ibWallForces = (-Sfp/magSfp) & totalStressIB;
+    Info<<"ibWallForces size:"<<ibWallForces.size()<<endl;
+
+}
+
+Foam::word Foam::NurbsStructureInterface::getXMLPath()
 {
     fileName caseDirectory = runDirectory+"/"+caseName;
     fileName constantDirectory = caseDirectory;
@@ -57,14 +169,14 @@ Foam::word Foam::NurbsStructure::getXMLPath()
     return fullPath;
 }
 
-int Foam::NurbsStructure::loadRodsFromXML()
+int Foam::NurbsStructureInterface::loadRodsFromXML()
 {
     std::string rodsXMLFilePath = xmlPath;
     ActiveRodMesh::import_xmlCrv(rodsList, rodsXMLFilePath, 3, 1, 0);
     return rodsList.size();
 }
 
-void Foam::NurbsStructure::createNurbsStructure()
+void Foam::NurbsStructureInterface::createNurbsStructure()
 {
 // **** Loading curves from file **** //
 	std::string name = "BCC_3x3x3_L20";
@@ -344,7 +456,7 @@ void Foam::NurbsStructure::createNurbsStructure()
 
 }
 
-Foam::NurbsStructure::~NurbsStructure()
+Foam::NurbsStructureInterface::~NurbsStructureInterface()
 {
     for (int i = 0; i < nR; i++)
 	{
