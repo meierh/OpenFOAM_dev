5cfea458fc2e689798bfdd97c11fc97011854ff0
Helge Meier <hmelbtal@t-online.de>
2024-11-22 00:46:49 +0100

    Added compile script

---
+++ wmake_all.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+cd src/structure
+rm -rf lnInclude Make/linux64GccDPInt32Opt
+wmake -j 12 &
+
+cd ../meshRefiner
+rm -rf lnInclude Make/linux64GccDPInt32Opt
+wmake -j 3 &
+
+cd ../interaction
+rm -rf lnInclude Make/linux64GccDPInt32Opt
+wmake -j 12 &
+
+cd ../optimizer
+rm -rf lnInclude Make/linux64GccDPInt32Opt
+wmake -j 3 &
+
+cd ../icoImmersedBoundary
+rm -rf lnInclude Make/linux64GccDPInt32Opt
+wmake -j 4 &
+
+cd ../icoAdjointImmersedBoundary
+rm -rf lnInclude Make/linux64GccDPInt32Opt
+wmake -j 8 &
+
+cd ../../applications/solvers/icoIB
+rm -rf Make/linux64GccDPInt32Opt
+wmake -j 3 &
+
+cd ../optimIB
+rm -rf Make/linux64GccDPInt32Opt
+wmake -j 3 &
+
+wait
+cd ../../../
+
+cd src/structure
+wmake &
+
+cd ../meshRefiner
+wmake &
+
+cd ../interaction
+wmake &
+
+cd ../optimizer
+wmake &
+
+cd ../icoImmersedBoundary
+wmake &
+
+cd ../icoAdjointImmersedBoundary
+wmake &
+
+cd ../../applications/solvers/icoIB
+wmake &
+
+cd ../optimIB
+wmake &
+
+wait
+echo "Completed compilation"
