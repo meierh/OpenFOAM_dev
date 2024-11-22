6523761422b514f1ddafbea926f718c7c8db6581
Helge Meier <hmelbtal@t-online.de>
2024-11-22 01:30:12 +0100

    Removed warnings

--- wmake_all_clean.sh
+++ wmake_all_clean.sh
@@ -1,27 +1,35 @@
 #!/bin/bash
 
 cd src/structure
+rm -rf lnInclude Make/linux64GccDPInt32Opt
 wmake -j 12 &
 
 cd ../meshRefiner
+rm -rf lnInclude Make/linux64GccDPInt32Opt
 wmake -j 3 &
 
 cd ../interaction
+rm -rf lnInclude Make/linux64GccDPInt32Opt
 wmake -j 12 &
 
 cd ../optimizer
+rm -rf lnInclude Make/linux64GccDPInt32Opt
 wmake -j 3 &
 
 cd ../icoImmersedBoundary
+rm -rf lnInclude Make/linux64GccDPInt32Opt
 wmake -j 4 &
 
 cd ../icoAdjointImmersedBoundary
+rm -rf lnInclude Make/linux64GccDPInt32Opt
 wmake -j 8 &
 
 cd ../../applications/solvers/icoIB
+rm -rf Make/linux64GccDPInt32Opt
 wmake -j 3 &
 
 cd ../optimIB
+rm -rf Make/linux64GccDPInt32Opt
 wmake -j 3 &
 
 wait
