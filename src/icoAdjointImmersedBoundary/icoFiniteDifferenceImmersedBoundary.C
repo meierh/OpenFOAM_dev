d38c0577ababd42a30ad9433f47bfbfa18b80aa7
Helge Meier <hmelbtal@t-online.de>
2025-07-25 00:47:53 +0200

    PC code

--- src/icoAdjointImmersedBoundary/icoFiniteDifferenceImmersedBoundary.C
+++ src/icoAdjointImmersedBoundary/icoFiniteDifferenceImmersedBoundary.C
@@ -92,3 +92,59 @@ void Foam::solvers::icoFiniteDifferenceImmersedBoundary::Solve()
         }
     }
 }
+
+void Foam::solvers::icoFiniteDifferenceImmersedBoundary::SolveEps()
+{
+    scalar fdGradient;
+    std::vector<scalar> J_eps;
+    //scalar writeTime = 1;
+    
+    Info<<"epsilons:";
+    for(scalar eps : epsilons)
+        Info<<eps<<" ";
+
+    std::unique_ptr<Time> timePtr = createTime(args);
+    std::unique_ptr<fvMesh> meshPtr = createMesh(*timePtr);
+    auto icoSolver = std::unique_ptr<icoAdjointImmersedBoundary>(new icoAdjointImmersedBoundary(*meshPtr,*timePtr,{para}));
+
+    for(scalar eps : epsilons)
+    {
+        Info<<"Start eps:"<<eps<<Foam::nl;
+        for(label sign : {1,-1})
+        {
+            std::unique_ptr<LineStructure>& structure = icoSolver->getStructure();
+            structure->setParameterValue(para,{parameterIniValue+(static_cast<scalar>(sign)*eps)});
+            structure->reInitializeMarkers(false,false);
+            scalarList values = structure->getParameterValue(para);
+            if(values.size()!=1)
+                FatalErrorInFunction<<"Parameter must be a single coefficient"<<exit(FatalError);
+            Info<<"Parameter set to:"<<values[0]<<Foam::nl;
+            icoSolver->SolvePrimalRepeated([&](bool completed, const Time& runTime, Time& time)
+            {
+                /*
+                if(false && completed)
+                {
+                    scalar convTime = time.userTimeValue();
+                    writeTime = std::max(convTime,writeTime);
+                    label intWriteTime = writeTime;
+                    intWriteTime+=1;
+                    writeTime = intWriteTime;
+                    scalar deltaTMissing = writeTime-convTime;
+                    time+=(deltaTMissing);
+                    Pout<<"Print into timestep set to:"<<writeTime<<" being:"<<time.userTimeValue()<<" from "<<convTime<<Foam::endl;
+                    runTime.write();
+                }
+                */
+            });
+            J_eps.push_back(J(*icoSolver));
+            Info<<"Done single instance "<<sign<<" eps:"<<eps<<Foam::nl;
+        }
+        fdGradient = (J_eps[0]-J_eps[1])/(2*eps);
+        if(Pstream::master())
+        {
+            Info<<"val:"<<parameterIniValue<<"  eps:"<<eps<<"  +eps J:"<<J_eps[0]<<"  -eps J:"<<J_eps[1]<<"  fdgrad:"<<fdGradient<<Foam::endl;
+            (*recordFDFile)<<"val:"<<parameterIniValue<<"  eps:"<<eps<<"  +eps J:"<<J_eps[0]<<"  -eps J:"<<J_eps[1]<<"  fdgrad:"<<fdGradient<<std::endl;
+
+        }
+    }
+}
